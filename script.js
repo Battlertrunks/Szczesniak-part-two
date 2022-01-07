9ffe068ea3c0cade033f054ade8bee8a2bc51b19
Gavin <Gavinszczesniak@gmail.com>
2022-01-07 18:40:50 -0500

    Wrote in JavaScript to ask for the player's name, how many wholes, and the number of putts that player has done. All being stored in a variable.

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("hello world!");
+const mini_Golf_Game = () => {
+  const name = prompt("Welcome to GC mini-golf! What is your name?");
+  const holes = prompt(
+    "Hi, " + name + "! Would you like to play 3 or 6 holes?"
+  );
+
+  let putts = [];
+
+  for (let i = 0; i < holes; i++) {
+    putts.push(prompt("How many putts for hole " + (i + 1) + "?"));
+  }
+  console.log(putts);
+};
+
+mini_Golf_Game();
