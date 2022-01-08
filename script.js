8d3c2312e48bb0a9466bea7e73c22d92969b9398
Gavin <Gavinszczesniak@gmail.com>
2022-01-07 19:09:17 -0500

    Golf Game is fully functional. I will add some code if the user deviates from options so the game doesn't break from wrong user input.

--- script.js
+++ script.js
@@ -7,9 +7,28 @@ const mini_Golf_Game = () => {
   let putts = [];
 
   for (let i = 0; i < holes; i++) {
-    putts.push(prompt("How many putts for hole " + (i + 1) + "?"));
+    putts.push(parseInt(prompt("How many putts for hole " + (i + 1) + "?")));
   }
   console.log(putts);
+
+  let score = 0;
+  if (holes == 3) {
+    score = -9;
+  } else {
+    score = -18;
+  }
+
+  for (let j = 0; j < putts.length; j++) {
+    score += putts[j];
+  }
+
+  if (score < 0)
+    console.log("Great job, " + name + "! Your total par was: " + score);
+  else if (score === 0)
+    console.log("Good game, " + name + ". Your total was: " + score);
+  else {
+    console.log("Nice try, " + name + ". Your total par was: +" + score);
+  }
 };
 
 mini_Golf_Game();
